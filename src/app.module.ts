ec738d44893daa3009e3dfc2785df68630c966cf
jacinto93 <jmz093@gmail.com>
2025-04-28 11:49:09 +1000

    finished #31 - added TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TweetModule, AuthModule],
+  imports: [
+    UsersModule,
+    TweetModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      synchronize: true,
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'root',
+      database: 'nestjs',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
