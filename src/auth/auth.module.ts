aeeac10d8ad4d361ce53ab65d7cc2d5846b5b5db
jacinto93 <jmz093@gmail.com>
2025-05-02 00:59:25 +1000

    finished #62 - module specific config / partial registration of config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,16 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import authConfig from './config/auth.config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    ConfigModule.forFeature(authConfig), // Load auth config
+    forwardRef(() => UsersModule),
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,25 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { UsersService } from 'src/users/users.service';
+import authConfig from './config/auth.config';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(forwardRef(() => UsersService))
     private readonly userService: UsersService,
+
+    @Inject(authConfig.KEY)
+    private readonly authConfiguration: ConfigType<typeof authConfig>,
   ) {}
 
   isAuthenticated: boolean = false;
 
   login(email: string, password: string) {
+    console.log('jm: auth config', this.authConfiguration);
     console.log('jm: attempting to login');
+    console.log('jm: email', email);
+    console.log('jm: password', password);
     // const user = this.userService.users.find(
     //   (u) => u.email === email && u.password === password,
     // );
